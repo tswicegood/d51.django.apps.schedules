6895f3607a62ee06831b592f8c10277be21843ca
Travis Swicegood <development@domain51.com>
2009-11-02 11:02:31 -0600

    Add ScheduleItem.objects.available() method
    This method grabs the current list of available schedule objects and returns
    the QuerySet.  You can filter it further by adding additional filter() calls
    before evaluating it.

--- d51/django/apps/schedules/models.py
+++ d51/django/apps/schedules/models.py
@@ -1,12 +1,19 @@
+import datetime
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
+
+class ScheduledItemManager(models.Manager):
+    def available(self):
+        return self.filter(published__lte=datetime.datetime.now())
 
 class ScheduledItem(models.Model):
     published = models.DateTimeField()
     object_id = models.PositiveIntegerField()
     content_type = models.ForeignKey(ContentType)
     content_object = generic.GenericForeignKey()
+
+    objects = ScheduledItemManager()
 
     class Meta(object):
         ordering = ['-published']
--- d51/django/apps/schedules/tests.py
+++ d51/django/apps/schedules/tests.py
@@ -7,9 +7,12 @@ import random
 # project.mysite available
 from project.mysite.models import Post
 
+def random_posts(number):
+    return [Post.objects.create(title=str(random.randint(1*i, 100*i))) for i in range(number)]
+
 class TestOfSchedule(TestCase):
     def test_schedules_default_to_reverse_chronological_order(self):
-        [a, b, c] = [Post.objects.create(title=str(random.randint(1*i, 100*i))) for i in range(1, 4)]
+        [a, b, c] = random_posts(3)
 
         now = datetime.now()
         ScheduledItem.objects.create(
@@ -30,4 +33,19 @@ class TestOfSchedule(TestCase):
         self.assertEqual(schedules[1].content_object, a)
         self.assertEqual(schedules[2].content_object, b)
 
+    def test_available_returns_models_that_are_available(self):
+        [a, b] = random_posts(2)
+        now = datetime.now()
+        ScheduledItem.objects.create(
+            published=now,
+            content_object=a
+        )
+        ScheduledItem.objects.create(
+            published=datetime(now.year+1, now.month, now.day),
+            content_object=b
+        )
+
+        expected = ScheduledItem.objects.get(object_id=a.pk)
+        self.assertEqual(1, ScheduledItem.objects.available().count())
+        self.assertEqual(expected, ScheduledItem.objects.available()[0])
 
