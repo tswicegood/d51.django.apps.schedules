46c6abbf8233b72e098fe9955186c7f98b1403a5
Travis Swicegood <development@domain51.com>
2009-12-29 15:58:07 -0600

    First pass at moving away from buildout to virtualenv and pip
    Definitely going to be a learning process, but this is my first attempt at
    moving entirely away from buildout.  Still need to document more thoroughly
    what's going on. and remove the buildout cruft.

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+from fabric.api import local
+
+def test():
+    """
+    Run tests for d51.django.apps.schedules
+    """
+    local("python ./run_tests.py")
+
+def init():
+    """
+    Initialize a virtualenv in which to run tests against this
+    """
+    local("virtualenv .")
+    local("pip install -E . -r requirements.txt")
+
